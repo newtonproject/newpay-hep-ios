9918cc3554c312d33a1e5f4898c6a0cb0ef7d0f2
Yiwei Heng <hengyiwei@diynova.com>
2019-06-24 10:46:25 +0800

    Update SDK for HEP

--- NewtonSDK/constant.swift
+++ NewtonSDK/constant.swift
@@ -12,33 +12,33 @@ struct Constants {
     static let NEWPAY = "newpay"
     static let NEWPAY_TEST = "newpay-test"
     static let NEWPAY_DEV = "newpay-dev"
-    
-    static let APPID = "app_id"
-    static let MESSAGE = "message"
+    static let DAPPID = "dappid"
+    static let MEMO = "memo"
     static let SIGN_R = "sign_r"
     static let SIGN_S = "sign_s"
-    static let PROTOCOL_VERSION = "protocol_version"
+    static let SIGNATURE = "signature"
+    static let SIGN_TYPE = "sign_type"
+    static let PROTOCOL_VERSION = "version"
+    static let PROTOCOL_NAME = "protocol"
     static let BUNDLE_SOURCE = "bundle_source"
     static let ENVIRONMENT = "environment"
     static let SCHEMA_PROTOCOL = "schema_protocol"
-    
     static let SCOPE = "scope"
-    
-    static let ORDER_JSON = "orderJson"
-    
-    static let SYMBOL = "symbol"
-    static let ADDRESS = "address"
-    static let AMOUNT = "amount"
-    static let SUMMARY = "summary"
-    
-    static let METHOD = "method"
-    static let REQUEST_PROFILE = "requestProfile"
-    static let PLACE_ORDER = "placeOrder"
-    static let PAY = "pay"
-    
-    
-    static let NEWMALLAPPID = "9a674d65c945569a9071b31b07f3bc52"
-    static let NEWMALLPRIVATE = "298b9bee0a0431e8f1a81323df6810b72467db21f9c252cb6d134d149005a386"
+    static let PROOF_HASH = "proof_hash"
+    static let DESCRIPTION = "description"
+    static let PRICE_CURRENCY = "price_currency"
+    static let TOTAL_PRICE = "total_price"
+    static let ORDER_NUMBER = "order_number"
+    static let SELLER = "seller"
+    static let CUSTOMER = "customer"
+    static let BROKER = "broker"
+    static let TS = "ts"
+    static let NONCE = "nonce"
+    static let UUID = "uuid"
+    static let ACTION = "action"
+    static let AUTH_LOGIN = "hep.auth.login"
+    static let PAY_ORDER = "hep.pay.order"
+    static let PROOF_SUBMIT = "hep.proof.submit"
 }
 
 
